00526a10d7b558fdd5124fe0ef8c6f702969889a
Dennis Chmielewski <chmielewski_dennis@hotmail.com>
2022-01-18 18:27:59 +0000

    fix admin page bug

--- mens_style_store/settings.py
+++ mens_style_store/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
