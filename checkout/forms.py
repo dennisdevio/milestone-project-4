14ca29ade3009be194b01ad7779331bf61a7e43b
Dennis Chmielewski <chmielewski_dennis@hotmail.com>
2022-02-01 12:18:54 +0000

    try fixing country field styling

--- checkout/forms.py
+++ checkout/forms.py
@@ -20,7 +20,6 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full name',
             'email': 'Email address',
             'phone_number': 'Phone number',
-            'country': 'Country',
             'postcode': 'Postal code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street address 1',
@@ -30,10 +29,11 @@ class OrderForm(forms.ModelForm):
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]}*'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]}*'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
