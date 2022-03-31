777c75b6b7dc5ad15f8386d64b200c11fa27ba40
Dennis Chmielewski <chmielewski_dennis@hotmail.com>
2022-03-31 07:31:07 +0000

    add checkout success views and template + update settings

--- checkout/views.py
+++ checkout/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
-from .models import OrderLineItem
+from .models import Order, OrderLineItem
 from products.models import Product
 from cart.contexts import cart_contents
 
 import stripe
+
+
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -71,7 +73,6 @@ def checkout(request):
         current_cart = cart_contents(request)
         total = current_cart['grand_total']
         stripe_total = round(total * 100)
-        print(stripe_secret_key)
         stripe.api_key = stripe_secret_key
         intent = stripe.PaymentIntent.create(
             amount=stripe_total,
